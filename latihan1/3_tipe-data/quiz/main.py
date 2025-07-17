a4c0b72bef048367e41787a667c48836a4c53324
dhanie <romadani.code@gmail.com>
2025-07-17 20:21:53 +0700

    quiz tipe data

---
+++ latihan1/3_tipe-data/quiz/main.py
@@ -0,0 +1,42 @@
+"""
+TODO:
+Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+- firstName: isi dengan nama depan Anda bertipe data string.
+- lastName: isi dengan nama belakang Anda bertipe data string.
+- age: isi dengan umur Anda bertipe data integer.
+- isMarried: isi dengan status pernikahan Anda bertipe data boolean.
+
+Catatan:
+- Value variabel harus berupa nilai sesungguhnya (literal) seperti string,
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+print("======================")
+firstName = "Roma"
+lastName = "Dani"
+age = 17
+isMarried = False
+
+print("nama saya :", firstName,lastName, "Umur saya :", age, "Status Menikah : ", isMarried)
+
+print("======================")
+
+"""
+TODO:
+Buatlah variabel dictionary dengan nama "data_diri",
+variabel tersebut berisi identitas diri Anda berdasarkan ketentuan berikut.
+- Memiliki key bernama "firstName":
+    - Isi value dengan nama depan Anda, pastikan bertipe data string.
+- Memiliki key bernama "lastName":
+    - Isi value dengan nama terakhir Anda, pastikan bertipe data string.
+- Memiliki key bernama "age":
+    - Isi value dengan umur Anda, pastikan bertipe data integer.
+- Memiliki key bernama "isMarried":
+    - Isi value dengan status pernikahan Anda, pastikan bertipe data boolean.
+
+Catatan:
+- Value pada dictionary harus berupa nilai sesungguhnya (literal) seperti string, 
+  bila
+"""
+x = {firstName: "Roma", lastName: "Dani", age: 17, isMarried: False}
+
+print(x)
